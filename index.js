75a5beaf7f9eef1be5dfa2442ce6abcf289d1a1e
hitesh-ydv <hiteshyadav124001@gmail.com>
2025-12-13 13:21:57 +0530

    add middlewares

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://cdn-services.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
